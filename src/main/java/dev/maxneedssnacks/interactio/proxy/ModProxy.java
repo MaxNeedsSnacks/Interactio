c1d7deafabb60a6ea3528e64ee723fb90c7bc106
Max <maxh2709@gmail.com>
2020-03-10 21:43:41 +0100

    Revert reload listener to fix server not loading datapacks

--- src/main/java/dev/maxneedssnacks/interactio/proxy/ModProxy.java
+++ src/main/java/dev/maxneedssnacks/interactio/proxy/ModProxy.java
@@ -1,13 +1,19 @@
 package dev.maxneedssnacks.interactio.proxy;
 
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 import dev.maxneedssnacks.interactio.command.CommandItemInfo;
 import dev.maxneedssnacks.interactio.command.CommandRegistryDump;
 import dev.maxneedssnacks.interactio.network.PacketCraftingParticle;
 import dev.maxneedssnacks.interactio.network.PacketInvalidateCache;
 import dev.maxneedssnacks.interactio.recipe.util.InWorldRecipeType;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.resources.JsonReloadListener;
 import net.minecraft.item.crafting.RecipeManager;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.resources.IResourceManager;
 import net.minecraft.server.MinecraftServer;
+import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SharedConstants;
 import net.minecraft.world.World;
 import net.minecraftforge.common.MinecraftForge;
@@ -16,7 +22,8 @@ import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.network.PacketDistributor;
-import net.minecraftforge.resource.ISelectiveResourceReloadListener;
+
+import java.util.Map;
 
 import static dev.maxneedssnacks.interactio.Interactio.NETWORK;
 
@@ -50,10 +57,13 @@ public abstract class ModProxy implements IProxy {
 
     private void serverAboutToStart(FMLServerAboutToStartEvent event) {
         server = event.getServer();
-        server.getResourceManager().addReloadListener((ISelectiveResourceReloadListener)
-                (resourceManager, resourceType) -> {
-                    InWorldRecipeType.clearCache();
-                    NETWORK.send(PacketDistributor.ALL.noArg(), new PacketInvalidateCache(true));
+        server.getResourceManager().addReloadListener(
+                new JsonReloadListener(new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create(), "recipes") {
+                    @Override
+                    protected void apply(Map<ResourceLocation, JsonObject> _0, IResourceManager _1, IProfiler _2) {
+                        InWorldRecipeType.clearCache();
+                        NETWORK.send(PacketDistributor.ALL.noArg(), new PacketInvalidateCache(true));
+                    }
                 }
         );
     }
