4673cafdcf7e7b14a999f10bd5da8bb2a3985471
Max <maxh2709@gmail.com>
2020-03-12 19:26:03 +0100

    Add Mixins, currently using them to clear recipe cache
    (Obviously, gonna use them for more soon-ish.)

---
+++ src/main/java/dev/maxneedssnacks/interactio/mixin/RecipeManagerMixin.java
@@ -0,0 +1,33 @@
+package dev.maxneedssnacks.interactio.mixin;
+
+import com.google.gson.JsonObject;
+import dev.maxneedssnacks.interactio.recipe.util.InWorldRecipeType;
+import net.minecraft.item.crafting.IRecipe;
+import net.minecraft.item.crafting.RecipeManager;
+import net.minecraft.profiler.IProfiler;
+import net.minecraft.resources.IResourceManager;
+import net.minecraft.util.ResourceLocation;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Map;
+
+@SuppressWarnings("unused")
+@Mixin(RecipeManager.class)
+public abstract class RecipeManagerMixin {
+
+    @Inject(method = "apply", at = @At("RETURN"))
+    public void clearCache(Map<ResourceLocation, JsonObject> map, IResourceManager resourceManager, IProfiler profiler, CallbackInfo ci) {
+        InWorldRecipeType.clearCache();
+    }
+
+    // This gets called by IClientPlayNetHandler#handleUpdateRecipes
+    // when handling SUpdateRecipesPacket packets.
+    @Inject(method = "func_223389_a", at = @At("RETURN"), remap = false)
+    public void clearCacheClient(Iterable<IRecipe<?>> recipes, CallbackInfo ci) {
+        InWorldRecipeType.clearCache();
+    }
+
+}
--- src/main/java/dev/maxneedssnacks/interactio/proxy/ModProxy.java
+++ src/main/java/dev/maxneedssnacks/interactio/proxy/ModProxy.java
@@ -1,25 +1,15 @@
 package dev.maxneedssnacks.interactio.proxy;
 
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonObject;
 import dev.maxneedssnacks.interactio.command.CommandItemInfo;
 import dev.maxneedssnacks.interactio.command.CommandRegistryDump;
 import dev.maxneedssnacks.interactio.recipe.util.InWorldRecipeType;
 import net.minecraft.client.Minecraft;
-import net.minecraft.client.resources.JsonReloadListener;
 import net.minecraft.item.crafting.RecipeManager;
-import net.minecraft.profiler.IProfiler;
-import net.minecraft.resources.IResourceManager;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.util.ResourceLocation;
 import net.minecraft.util.SharedConstants;
 import net.minecraft.world.World;
-import net.minecraftforge.client.event.RecipesUpdatedEvent;
 import net.minecraftforge.common.MinecraftForge;
-import net.minecraftforge.fml.event.server.FMLServerAboutToStartEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
-
-import java.util.Map;
 
 public abstract class ModProxy implements IProxy {
 
@@ -31,31 +21,13 @@ public abstract class ModProxy implements IProxy {
         InWorldRecipeType.registerTypes();
 
         // Forge Event Bus events
-        MinecraftForge.EVENT_BUS.addListener(this::serverAboutToStart);
         MinecraftForge.EVENT_BUS.addListener(this::serverStarting);
 
-        MinecraftForge.EVENT_BUS.addListener(this::recipesUpdated);
-    }
-
-    private void serverAboutToStart(FMLServerAboutToStartEvent event) {
-        server = event.getServer();
-        server.getResourceManager().addReloadListener(
-                new JsonReloadListener(new GsonBuilder().setPrettyPrinting().disableHtmlEscaping().create(), "recipes") {
-                    @Override
-                    protected void apply(Map<ResourceLocation, JsonObject> _0, IResourceManager _1, IProfiler _2) {
-                        InWorldRecipeType.clearCache();
-                    }
-                }
-        );
     }
 
     private void serverStarting(FMLServerStartingEvent event) {
         CommandItemInfo.register(event.getCommandDispatcher());
         CommandRegistryDump.register(event.getCommandDispatcher());
-    }
-
-    private void recipesUpdated(RecipesUpdatedEvent event) {
-        InWorldRecipeType.clearCache();
     }
 
     @Override
