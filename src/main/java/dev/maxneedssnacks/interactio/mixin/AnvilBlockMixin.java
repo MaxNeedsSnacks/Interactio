2a68416d2edc7982f4e59454a460180c0b4d3a36
Max <maxh2709@gmail.com>
2020-04-19 14:11:04 +0200

    Add item anvil smashing recipe type (fixes #9)
    JEI integration still needs to be tidied up, and a bit of testing should probably still be done before this is merged

---
+++ src/main/java/dev/maxneedssnacks/interactio/mixin/AnvilBlockMixin.java
@@ -0,0 +1,38 @@
+package dev.maxneedssnacks.interactio.mixin;
+
+import dev.maxneedssnacks.interactio.recipe.util.InWorldRecipe;
+import dev.maxneedssnacks.interactio.recipe.util.InWorldRecipeType;
+import net.minecraft.block.AnvilBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FallingBlock;
+import net.minecraft.entity.item.ItemEntity;
+import net.minecraft.util.math.AxisAlignedBB;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+
+@Mixin(AnvilBlock.class)
+public abstract class AnvilBlockMixin extends FallingBlock {
+
+    public AnvilBlockMixin(Properties p_i48401_1_) {
+        super(p_i48401_1_);
+        throw new IllegalStateException();
+    }
+
+    @Inject(
+            method = "onEndFalling(Lnet/minecraft/world/World;Lnet/minecraft/util/math/BlockPos;Lnet/minecraft/block/BlockState;Lnet/minecraft/block/BlockState;)V",
+            at = @At(value = "RETURN")
+    )
+    public void handleAnvilRecipes(World world, BlockPos pos, BlockState fallState, BlockState hitState, CallbackInfo ci) {
+        List<ItemEntity> items = world.getEntitiesWithinAABB(ItemEntity.class, new AxisAlignedBB(pos, pos.add(1, 1, 1)));
+        
+        InWorldRecipeType.ITEM_ANVIL_SMASHING.applyAll(recipe -> recipe.canCraft(items, hitState),
+                recipe -> recipe.craft(items, new InWorldRecipe.DefaultInfo(world, pos)));
+    }
+
+}
